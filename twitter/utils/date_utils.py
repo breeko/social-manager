088972c952577aa8c943083ce33a4b37173098a8
Branko <branko.blagojevic@gmail.com>
2020-09-02 18:40:52 -0400

    date_utils uses .now()

--- twitter/utils/date_utils.py
+++ twitter/utils/date_utils.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 DATE_FORMAT = "%Y-%m-%d %H:%M"
 
-def format_date(date: datetime = timezone.localtime()) -> str:
+def format_date(date: datetime = timezone.now()) -> str:
   """ converts a date to a string, defaults to now """
   return date.strftime(DATE_FORMAT)
 
@@ -14,6 +14,6 @@ def read_date(date: str) -> 'datetime':
   """ Converts a string to a date """
   return datetime.strptime(date, DATE_FORMAT)
 
-def within_hour(dt: datetime = timezone.localtime(), hours: float = 1.0) -> 'datetime':
+def within_hour(dt: datetime = timezone.now(), hours: float = 1.0) -> 'datetime':
   """ Returns a datetime object that's within in the next hour """
   return dt + timedelta(hours=random() * hours)
